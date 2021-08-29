01bd01639460fcb184b7d61f8a4b93a56175067d
larissabenedet <larissavb2014@outlook.com>
2021-08-29 00:14:00 -0300

    Alterando fonte e estilos, classe ativa no menu

--- buscacep/src/Components/Menu.js
+++ buscacep/src/Components/Menu.js
@@ -2,21 +2,27 @@ import React from "react";
 import styles from "./Menu.module.css";
 
 const Menu = () => {
+  const pathname = window.location.pathname;
+
   return (
     <nav className={styles.menu}>
       <div>
         <a href="/">
-          <h1>
-            <i>Busca</i>CEP
-          </h1>
+          <h1>BuscaCEP</h1>
         </a>
       </div>
       <div className={styles.navLinks}>
-        <a href="/">Inicial</a>
-        <a href="/favoritos">Favoritos</a>
+        <a href="/">
+          <span className={pathname === "/" && styles.ativo}>Inicial</span>
+        </a>
+        <a href="/favoritos">
+          <span className={pathname === "/favoritos" && styles.ativo}>
+            Favoritos
+          </span>
+        </a>
       </div>
     </nav>
   );
 };
 
-export default Menu
+export default Menu;
